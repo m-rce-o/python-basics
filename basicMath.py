079bd2bb99eb62761c2a22927a1419540015123c
m-rce-o <paraspamss@gmail.com>
2026-05-27 10:58:13 -0700

    Adding basic conditionals

--- basicMath.py
+++ basicMath.py
@@ -2,5 +2,12 @@ x = 54321 + 456813
 y = 150 / 6
 z = 2**2
 
-print(x + y)
-print(z - y)
+# print(x + y)
+# print(z - y)
+
+x = int(input("Enter your number: "))
+
+if x >= 100:
+    print("That's a big number")
+else:
+    print("That's a small number")
---
+++ strings.py
@@ -0,0 +1,5 @@
+first = "I am part one..."
+second = " and I am part two!"
+
+fullStr = first + second
+print(fullStr)
